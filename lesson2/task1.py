44a00b4c27dd247f7c2d9d912a4c42be4804ae13
Roman Makuta <eee127@yandex.ru>
2022-02-17 21:13:23 +0300

    added headers and found vacancy items

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.hh.ru"
+vacancy = input("Please enter vacancy: ")
 
-response = requests.get(url)
+url = f"https://hh.ru/search/vacancy?area=1&fromSearchLine=true&text={vacancy}"
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"}
+response = requests.get(url, headers=headers)
 
 dom = BeautifulSoup(response.text, 'html.parser')
+
+vacancy_items = dom.find_all("div", {"class": "vacancy-serp-item"})
+print(len(vacancy_items))
